d93018c407bd8ea2a856da126682a6bd0ac8da53
drinkingtheink <jason.m.harrison@gmail.com>
2023-07-06 18:26:53 -0500

    design sys skills

--- src/components/AppStage.vue
+++ src/components/AppStage.vue
@@ -277,7 +277,7 @@ export default {
       ],
       designSysText: 'Built-in consistent branding and functionality...',
       designSysSkills: [
-        'Web Components', 'Stencil', 'Lit', 'Accessibility', 'Semantic Markup', 'Web APIs', `Shadow DOM`, `CSS Vars`,
+        'Web Components', 'Stencil', 'Lit', 'Accessibility', 'Semantic Markup', 'Web APIs', `Shadow DOM`, `CSS Vars`, 'Animation/Motion',
       ],
       designSysLinks: [
         {
