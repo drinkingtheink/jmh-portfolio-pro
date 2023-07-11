91a05787c6789527ae4a5eb789ce2c3a7df18665
drinkingtheink <jason.m.harrison@gmail.com>
2023-07-10 20:16:14 -0500

    dark mode support

--- src/components/AppStage.vue
+++ src/components/AppStage.vue
@@ -418,6 +418,13 @@ $appWidth: 1200px;
 $boxShadow: 0px 3px 5px 3px rgba(0,0,0,0.36);
 $boxShadowDark: 0px 3px 10px 3px rgba(0,0,0,0.8);
 
+:root {
+  --white: #fff;
+  --bgColor: var(--white);
+  --darkGrey: #222;
+  --fontColor: var(--darkGrey);
+}
+
 body {
   background-color: $col2;
   background-image: url("data:image/svg+xml,%3Csvg width='100' height='20' viewBox='0 0 100 20' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M21.184 20c.357-.13.72-.264 1.088-.402l1.768-.661C33.64 15.347 39.647 14 50 14c10.271 0 15.362 1.222 24.629 4.928.955.383 1.869.74 2.75 1.072h6.225c-2.51-.73-5.139-1.691-8.233-2.928C65.888 13.278 60.562 12 50 12c-10.626 0-16.855 1.397-26.66 5.063l-1.767.662c-2.475.923-4.66 1.674-6.724 2.275h6.335zm0-20C13.258 2.892 8.077 4 0 4V2c5.744 0 9.951-.574 14.85-2h6.334zM77.38 0C85.239 2.966 90.502 4 100 4V2c-6.842 0-11.386-.542-16.396-2h-6.225zM0 14c8.44 0 13.718-1.21 22.272-4.402l1.768-.661C33.64 5.347 39.647 4 50 4c10.271 0 15.362 1.222 24.629 4.928C84.112 12.722 89.438 14 100 14v-2c-10.271 0-15.362-1.222-24.629-4.928C65.888 3.278 60.562 2 50 2 39.374 2 33.145 3.397 23.34 7.063l-1.767.662C13.223 10.84 8.163 12 0 12v2z' fill='%2306365c' fill-opacity='0.4' fill-rule='evenodd'/%3E%3C/svg%3E");
@@ -708,13 +715,14 @@ nav.large-nav {
 .panel {
   margin: 0 2rem 2rem 2rem;
   padding: 1rem 1rem 3rem 1rem;
-  background: rgba(255,255,255,1);
+  background: var(--bgColor);
   border-radius: 10px;
   min-height: 20rem;
   box-shadow: $boxShadow;
   display: flex;
   position: relative;
   z-index: 9;
+  color: var(--fontColor);
 
   .left-pane {
     padding: 0 2rem;
@@ -775,6 +783,10 @@ nav.large-nav {
       margin-top: 0;
       color: rgba(29, 13, 13, 0.6);
       text-transform: uppercase;
+
+      @media screen and (prefers-color-scheme: dark) {
+        color: palegreen;
+      }
     }
 
     p {
@@ -810,11 +822,26 @@ nav.large-nav {
   .right-pane::-webkit-scrollbar-track {
     background: rgba(0, 0, 0, 0.3);
     border-radius: 10px;
+
+    @media screen and (prefers-color-scheme: dark) {
+      background: rgba(255, 255, 255, 0.3);
+    }
   }
 
   .right-pane::-webkit-scrollbar-thumb {
     background-color: rgba(0, 0, 0, 0.8);
     border-radius: 10px;
+
+    @media screen and (prefers-color-scheme: dark) {
+      background: rgba(255, 255, 255, 0.8);
+    }
+  }
+}
+
+@media screen and (prefers-color-scheme: dark) {
+  .panel {
+      --bgColor: var(--darkGrey);
+      --fontColor: var(--white);
   }
 }
 
